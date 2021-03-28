f6b7da095e1b724993123997b0a95dec88f80b6c
Akaguara <akaguara@gmail.com>
2021-03-28 12:01:43 +0300

    add a file loader

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,17 @@ module.exports = {
 module = {
     rules: [
             {
-        test: '/\.js$/',
-        exclude: '/node_modules/',
-        loader: "babel-loader"
+              test: '/\.js$/',
+              exclude: '/node_modules/',
+              loader: "babel-loader"
+            },
+            {
+              test: '/\.(png|jpe?g|gif)$/i',
+              use: [
+                {
+                  loader: 'file-loader',
+                },
+              ]
             }
-    ]
-}
+          ]
+        }
